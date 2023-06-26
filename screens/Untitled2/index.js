a643172af4e0de4b922f233eeb44555a5ceb8574
Ravinder sales <ravinder.y@crowdbotics.com>
2023-06-26 14:18:34 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,20 +1,68 @@
-import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from "react-native";
 
-const Untitled2 = () => {
-  return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
-      padding: 10,
-      position: 'relative',
-      flex: 1
-    }}></ScrollView>
-    </SafeAreaView>;
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {// TODO: Implement login logic here
+    // 1. Validate email and password
+    // 2. Make API call to authenticate user
+    // 3. Navigate to home screen if successful
+    // 4. Display error message if unsuccessful
+  };
+
+  return <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} keyboardType="email-address" autoCapitalize="none" />
+      <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.instructions}>
+        Enter your email and password to login.
+      </Text>
+    </View>;
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    height: '100%'
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 20
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  instructions: {
+    marginTop: 20,
+    color: "#999",
+    fontSize: 16,
+    textAlign: "center"
   }
 });
-export default Untitled2;
+export default LoginScreen;
