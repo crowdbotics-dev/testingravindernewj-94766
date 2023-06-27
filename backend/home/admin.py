a6867150b90ec77349795d958d5c2bbac69bbebc
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-27 06:02:54 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CRFtoken
+admin.site.register(CRFtoken)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class CRFtoken(models.Model):
+    'Generated Model'
+    crftoken = models.BigIntegerField()
